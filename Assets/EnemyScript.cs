5cdc2d000fa774067d07337a42e4c3034ff491c8
Koorosh <koorosh.m@outlook.com>
2022-03-06 21:54:10 +0330

    Adding enemy
    Enemy object created, chases player, has attack timer and hit damage and can kill player.
    to do: enemy prefam and instantiate, enemy health system and death

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    Animator anim;
+    [SerializeField] Transform player;
+    [SerializeField] float walkSpeed = 1f;
+    Rigidbody2D rb2d;
+    public Transform attackPoint;
+    public float attackRange;
+    public int damage;
+    public LayerMask playerLayer;
+    float timeBtwAttacks;
+    public float attackTimer;
+
+
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    
+    void Update()
+    {
+        timeBtwAttacks -= Time.deltaTime;        
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+        if(distanceToPlayer < 0.9f){
+            anim.SetBool("Walking", false);
+            rb2d.velocity = new Vector2(0, 0);
+            if (timeBtwAttacks <= 0) {
+                EnemyAttack();
+            }
+        } else {
+            ChasePlayer();
+            }
+
+    }
+
+    void ChasePlayer(){
+        anim.SetBool("Walking", true);
+        if (transform.position.x < player.position.x) {
+            rb2d.velocity = new Vector2 (walkSpeed, 0);
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        } else {
+            rb2d.velocity = new Vector2(-walkSpeed, 0);
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+
+    void EnemyAttack(){
+        
+        anim.SetTrigger("Attack");
+        Collider2D colliderInfo = Physics2D.OverlapCircle(attackPoint.position, attackRange, playerLayer);
+        colliderInfo.GetComponent<PlayerMovement>().TakeDamage(damage);
+        timeBtwAttacks = attackTimer;
+    }   
+
+    // private void OnDrawGizmosSelected() {
+    //     Gizmos.color = Color.red;
+    //     Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    // }
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public int health = 100;
     public CharacterController2D controller2D;
     private float xMove;
     [SerializeField] protected float moveSpeed = 20f;
@@ -28,4 +29,14 @@ public class PlayerMovement : MonoBehaviour
         controller2D.Move(xMove * Time.fixedDeltaTime, false, jump);
         jump = false;
     }
+
+    public void TakeDamage(int damage){
+        health -= damage;
+        if (health < 0){
+            Destroy(gameObject);
+        }
+    }
+
+
 }
+
